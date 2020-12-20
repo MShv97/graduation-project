5a12c70333a697f65942b32ec473ab46618ee4a2
MShv97 <mshv97@gmail.com>
2020-12-20 19:09:30 +0200

    change typeORM config

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
+module.exports = {
   "type": "mysql",
-  "host": "localhost",
-  "port": 3306,
-  "username": "root",
-  "password": "password",
-  "database": "mymenu",
+  "host": process.env.TYPEORM_HOST,
+  "port": process.env.TYPEORM_PORT,
+  "username": process.env.TYPEORM_USERNAME,
+  "password": process.env.TYPEORM_PASSWORD,
+  "database": process.env.TYPEORM_DATABASE,
   "synchronize": true,
   "logging": false,
   "entities": ["src/entities/**/*.ts"],
