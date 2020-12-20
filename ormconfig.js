a0305cf3f9ec8c280d0fbba0458a281d0d48a477
MShv97 <mshv97@gmail.com>
2020-12-20 19:59:44 +0200

    bug fix

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
   "type": "mysql",
   "host": process.env.TYPEORM_HOST,
-  "port": process.env.TYPEORM_PORT,
+  "port": Number(process.env.TYPEORM_PORT),
   "username": process.env.TYPEORM_USERNAME,
   "password": process.env.TYPEORM_PASSWORD,
   "database": process.env.TYPEORM_DATABASE,
-  "synchronize": true,
+  "synchronize": false,
   "logging": false,
   "entities": ["src/entities/**/*.ts"],
   "migrations": ["src/migration/**/*.ts"],
