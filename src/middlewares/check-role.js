4ec7f27adef2d175f1fa740c316addcff371c613
MShv97 <mshv97@gmail.com>
2021-01-10 23:11:49 +0200

    MM-20

--- src/middlewares/check-role.js
+++ src/middlewares/check-role.js
@@ -11,7 +11,8 @@ module.exports = (roles) => {
     // get the user  req
     const { user } = req;
     // check if has any of the given roles
-    if (roles.indexOf(user.role) > -1) next();
+    if (user.role == "restaurant") next();
+    else if (roles.indexOf(user.role) > -1) next();
     else
       res.status(401).send({
         message: "You are not allowed to do this request.",
